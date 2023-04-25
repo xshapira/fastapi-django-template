90c306796e0d475f6274adf45868aa966a5015a2
Max Shapira <maxshapira9@gmail.com>
2023-04-25 15:12:46 +0300

    create User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """User model."""
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+    posts = models.ManyToManyField("posts.BlogPost", related_name="authors", blank=True)
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
