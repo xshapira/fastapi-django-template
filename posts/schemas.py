f582e86d93da4bcee15b612f3c4cacf30b7097b9
Max Shapira <maxshapira9@gmail.com>
2023-04-26 12:17:44 +0300

    create pydantic schema for BlogPost model

--- posts/schemas.py
+++ posts/schemas.py
@@ -1,8 +1,25 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
+from posts.models import BlogPost
 
-class Post(BaseModel):
+
+class BlogPostBase(BaseModel):
     title: str
+    slug: str
+    authors: list[str]
     content: str
-    published: bool = True
-    rating: int | None = None
+
+
+class BlogPostOutput(BlogPostBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        model = BlogPost
+
+
+class BlogPostUpdate(BlogPostBase):
+    pass
