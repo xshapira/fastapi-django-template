fb2257a64a1dbe93ccab893ca2d10a49687b363e
Max Shapira <maxshapira9@gmail.com>
2023-04-28 15:51:26 +0300

    add unique email validation using django's IntegrityError

--- users/router.py
+++ users/router.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from asgiref.sync import sync_to_async
 from django.contrib.auth.hashers import make_password
+from django.db import IntegrityError
 from fastapi import APIRouter, HTTPException
 
 from users.models import User
@@ -22,23 +23,29 @@ def get_users() -> list[UserOutput]:
 
 @router.get("/{user_id}", response_model=UserOutput)
 def get_user(user_id: int) -> Any:
-    return User.objects.get(id=user_id)
+    user = User.objects.get(id=user_id)
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user
 
 
 @router.post("/", response_model=UserOutput)
 async def create_user(user: UserCreate):
-    # Check if the email is unique
-    email_is_unique = (
-        await sync_to_async(User.objects.filter(email=user.email).exists)(),
-    )
-    if email_is_unique:
-        raise HTTPException(status_code=400, detail="Email address already exists")
-
     # Hash the password
     hashed_password = make_password(user.password.get_secret_value())
-    new_user = await sync_to_async(User.objects.create)(
-        name=user.name,
-        email=user.email,
-        password=hashed_password,
-    )
+
+    try:
+        new_user = await sync_to_async(User.objects.create)(
+            name=user.name,
+            email=user.email,
+            password=hashed_password,
+        )
+    except IntegrityError as e:
+        raise HTTPException(
+            status_code=409,
+            detail="Email address already exists.",
+        ) from e
+
     return new_user
