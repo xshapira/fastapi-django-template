1cb63d4ad3929b50a4c3180ae6889e532b73ea88
Max Shapira <maxshapira9@gmail.com>
2023-04-28 10:19:17 +0300

    refactor get_users to use UserOutput.from_orms for queryset conversion

--- users/router.py
+++ users/router.py
@@ -12,25 +12,17 @@ router = APIRouter()
 
 @router.get("/", response_model=list[UserOutput])
 def get_users() -> list[UserOutput]:
-    return User.objects.all()
+    # We need to convert the queryset of Django models into a list of
+    # UserOutput objects. We can achieve this by using the from_orm method
+    # we've defined in the UserOutput class.
+
+    user_queryset = User.objects.all()
+    return UserOutput.from_orms(user_queryset)
 
 
 @router.get("/{user_id}", response_model=UserOutput)
 def get_user(user_id: int) -> Any:
     return User.objects.get(id=user_id)
-
-
-# @router.post("/", response_model=UserOutput)
-# def create_user(user: UserCreate):
-#     # Hash the password
-#     hashed_password = make_password(user.password.get_secret_value())
-#     new_user = User.objects.create(
-#         name=user.name,
-#         email=user.email,
-#         password=hashed_password,
-#     )
-#     print("New user created:", new_user)
-#     return new_user
 
 
 @router.post("/", response_model=UserOutput)
@@ -50,41 +42,3 @@ async def create_user(user: UserCreate):
         password=hashed_password,
     )
     return new_user
-
-
-# @router.put("/{user_id}", response_model=UserOutput)
-# def update_user(user_id: int, input: UserUpdate, user: User) -> Any:
-#     user = user.objects.get(id=user_id)
-
-#     if user is None:
-#         raise HTTPException(status_code=404, detail="User not found.")
-
-#     try:
-#         for key, value in input.dict(exclude_unset=True).items():
-#             setattr(user, key, value)
-#         db.commit()
-#     except IntegrityError as e:
-#         raise HTTPException(status_code=409, detail="Email already exists.") from e
-#     return user
-
-
-# @router.delete("/{user_id}", response_model=UserOutput)
-# def delete_user(user_id: int, db: Annotated[QuerySet[User], Depends(get_db)]) -> Any:
-#     user = db.get(id=user_id)
-
-#     if user is None:
-#         raise HTTPException(status_code=404, detail="User not found.")
-
-#     db.delete(user)
-#     db.commit()
-
-#     return JSONResponse(
-#         content={"message": "User deleted successfully"}, status_code=200
-#     )
-
-
-# # This is a new endpoint that was added to the API.
-# @router.get("/users/{user_id}/posts", response_model=list[BlogPostOutput])
-# def get_user_posts(user_id: int, db: Annotated[QuerySet[User], Depends(get_db)]) -> Any:  # noqa: E501
-#     posts = db.filter(User.id == user_id).values("posts__title").distinct()
-#     return posts
