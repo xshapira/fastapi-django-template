87e69a9443442268ff4098cc3c679bfda0c91e82
Max Shapira <maxshapira9@gmail.com>
2023-04-28 10:42:39 +0300

    add ForeignKey for BlogPost in User model

--- users/models.py
+++ users/models.py
@@ -8,12 +8,12 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
-    # posts = models.ForeignKey(
-    #     "posts.BlogPost",
-    #     blank=True,
-    #     on_delete=models.CASCADE,
-    #     related_name="blog_authors",
-    # )
+    posts = models.ForeignKey(
+        "posts.BlogPost",
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="blog_authors",
+    )
 
     class Meta:
         verbose_name = "User"
